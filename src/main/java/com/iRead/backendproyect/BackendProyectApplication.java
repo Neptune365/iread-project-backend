53e14ee0a75d319cc9b1b008e3794094286cb171
Guillermo Sanchez <lsanchezp5@upao.edu.pe>
2023-10-10 02:11:07 -0500

    agregar plantilla del proyecto

---
+++ src/main/java/com/iRead/backendproyect/BackendProyectApplication.java
@@ -0,0 +1,13 @@
+package com.iRead.backendproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iRead/backendproyect/BackendProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iRead.backendproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
